e46285b352fdcc1dcdf8dcc4af20d8c5f9a21d7f
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-07-31 18:00:34 +0200

    Fealure is not an option

--- lib.py
+++ lib.py
@@ -43,13 +43,10 @@ def readlog():
         i += 1
 '''
 
-def readlog():    
-    f = open('/tmp/svxreflector.log')
-    i = 0
-    for x in f:
-        e = x.split(':')
-        name = e[3].strip()
-        if 'Login' in x:
-            s.prov[name] = e[4][15:]
+def readlog():
+    with open('/tmp/svxreflector.log') as f
+        line = f.readlines()
+        if 'Login' in line:
+            element = line.split(':')
+            s.prov[element[3].strip()] = element[4][15:]
 
-    f.close()
